e016bf5a123bd24fd081ca14700d7474945b3f4e
Henrique Bastos <henrique@bastos.net>
2019-09-05 01:10:50 -0300

    Desenrolando  o bug  que Helinho descobriu

--- json2csv/core.py
+++ json2csv/core.py
@@ -3,6 +3,7 @@ Author: Henrique Bastos <henrique@bastos.net>
 Based on the post from Amir Ziai
 https://towardsdatascience.com/flattening-json-objects-in-python-f5343c794b10
 """
+from json2csv.datastructures import MultiValueDict
 
 
 def flatten(obj, path=(), sep='__'):
@@ -11,7 +12,10 @@ def flatten(obj, path=(), sep='__'):
             yield from flatten(v, path + (k,))
     elif isinstance(obj, list):
         for i, v in enumerate(obj):
-            yield from flatten(v, path + (str(i),))
+            if isinstance(v, dict):
+                yield from flatten(v, path)
+            else:
+                yield from flatten(v, path + (str(i),))
     else:
         yield sep.join(path), obj
 
@@ -29,3 +33,55 @@ if __name__ == '__main__':
 
     assert (dict(flatten({'b': {'c': [2, 3], 'd': {'e': 4, 'f': 5}}}))
             == {'b__c__0': 2, 'b__c__1': 3, 'b__d__e': 4, 'b__d__f': 5})
+
+    assert dict(flatten([1, 2, 3])) == {'0': 1, '1': 2, '2': 3}
+
+    json = (
+        [
+            {"time": "Flamengo",
+             "jogadores": [
+                 {"nome": "Junior Baiano"},
+                 {"nome": "Gilmar"},
+                 {"nome": "Leão"},
+             ],
+             },
+            {"time": "Sport",
+             "jogadores":
+                 [
+                     {"nome": "Magrão"},
+                     {"nome": "Juninho"},
+                     {"nome": "Bala"},
+                 ],
+             },
+            {"time": "Corinthians",
+             "jogadores":
+                 [
+                     {"nome": "Socrates"},
+                     {"nome": "Casa Grande"},
+                     {"nome": "Cássio"},
+                 ],
+             },
+        ]
+    )
+
+    csv = (
+        [
+            {"time": "Flamengo", "jogadores__nome": "Junior Baiano"},
+            {"time": "Flamengo", "jogadores__nome": "Gilmar"},
+            {"time": "Flamengo", "jogadores__nome": "Leão"},
+            {"time": "Sport", "jogadores__nome": "Magrão"},
+            {"time": "Sport", "jogadores__nome": "Juninho"},
+            {"time": "Sport", "jogadores__nome": "Bala"},
+            {"time": "Corinthians", "jogadores__nome": "Socrates"},
+            {"time": "Corinthians", "jogadores__nome": "Casa Grande"},
+            {"time": "Corinthians", "jogadores__nome": "Cássio"},
+        ]
+    )
+
+    m = MultiValueDict()
+    for k, v in flatten(json):
+        #m.appendlist(k, v)
+        print(k, v)
+
+    #print(m)
+    #assert defaultdict(list, flatten(json)) == csv
