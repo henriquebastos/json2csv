fa02d7b8d1fedac791f4e549325f7821d4f3b866
Henrique Bastos <henrique@bastos.net>
2019-09-06 12:09:06 -0300

    Tabulandoa  bagaça

--- json2csv/core.py
+++ json2csv/core.py
@@ -3,54 +3,81 @@ Author: Henrique Bastos <henrique@bastos.net>
 Based on the post from Amir Ziai
 https://towardsdatascience.com/flattening-json-objects-in-python-f5343c794b10
 """
+from itertools import count, chain
+
 from json2csv.datastructures import MultiValueDict
 
 
-def flatten(obj, path=(), sep='__'):
+def flatten(obj, path=()):
     if isinstance(obj, dict):
         for k, v in obj.items():
             yield from flatten(v, path + (k,))
     elif isinstance(obj, list):
-        for i, v in enumerate(obj):
-            if isinstance(v, dict):
-                yield from flatten(v, path)
-            else:
+        if all(isinstance(item, dict) for item in obj):
+            for item in obj:
+                yield from flatten(item, path)
+        else:
+            for i, v in enumerate(obj):
                 yield from flatten(v, path + (str(i),))
     else:
-        yield sep.join(path), obj
+        yield path, obj
+
+
+def tabulate(iterable, sep='__'):
+    row = {}
+    last_level = level = 0
+
+    for path, value in iterable:
+        level = len(path)
+        header = sep.join(path)
+
+        if level == last_level and header in row:
+            yield row.copy()
+        elif level < last_level:
+            yield row.copy()
+            for n in range(last_level - level):
+                row.popitem()
+
+        row[header] = value
+
+        last_level = level
+    yield row
+
+def trim(d, level):
+    return {k: v for k, v in d.items() if len(k) < level}
 
 
 if __name__ == '__main__':
     # tests
     assert (dict(flatten({'a': 1, 'b': 2, 'c': 3}))
-            == {'a': 1, 'b': 2, 'c': 3})
+            == {('a',): 1, ('b',): 2, ('c',): 3})
 
     assert (dict(flatten({'a': 1, 'b': [2, 3], 'c': 4}))
-            == {'a': 1, 'b__0': 2, 'b__1': 3, 'c': 4})
+            == {('a',): 1, ('b', '0'): 2, ('b', '1'): 3, ('c',): 4})
 
     assert (dict(flatten({'a': 1, 'b': {'c': 2, 'd': 3}, 'e': 4}))
-            == {'a': 1, 'b__c': 2, 'b__d': 3, 'e': 4})
+            == {('a',): 1, ('b', 'c'): 2, ('b', 'd'): 3, ('e',): 4})
 
     assert (dict(flatten({'b': {'c': [2, 3], 'd': {'e': 4, 'f': 5}}}))
-            == {'b__c__0': 2, 'b__c__1': 3, 'b__d__e': 4, 'b__d__f': 5})
+            == {('b', 'c', '0'): 2, ('b', 'c', '1'): 3, ('b', 'd', 'e'): 4, ('b', 'd', 'f'): 5})
 
-    assert dict(flatten([1, 2, 3])) == {'0': 1, '1': 2, '2': 3}
+    assert dict(flatten([1, 2, 3])) == {('0',): 1, ('1',): 2, ('2',): 3}
 
     json = (
         [
             {"time": "Flamengo",
              "jogadores": [
-                 {"nome": "Junior Baiano"},
-                 {"nome": "Gilmar"},
-                 {"nome": "Leão"},
+                 {"nome": "Junior Baiano", "idade": 40},
+                 {"nome": "Gilmar", "idade": 50},
+                 {"nome": "Leão", "idade": 60},
              ],
              },
             {"time": "Sport",
              "jogadores":
                  [
-                     {"nome": "Magrão"},
-                     {"nome": "Juninho"},
-                     {"nome": "Bala"},
+                     {"nome": "Magrão", "idade": 45},
+                     {"nome": "Juninho", "idade": 55},
+                     {"nome": "Bala", "idade": 65},
                  ],
              },
             {"time": "Corinthians",
@@ -58,30 +85,36 @@ if __name__ == '__main__':
                  [
                      {"nome": "Socrates"},
                      {"nome": "Casa Grande"},
-                     {"nome": "Cássio"},
+                     {"nome": "Cássio", "idade": 40},
                  ],
              },
         ]
     )
 
-    csv = (
-        [
-            {"time": "Flamengo", "jogadores__nome": "Junior Baiano"},
-            {"time": "Flamengo", "jogadores__nome": "Gilmar"},
-            {"time": "Flamengo", "jogadores__nome": "Leão"},
-            {"time": "Sport", "jogadores__nome": "Magrão"},
-            {"time": "Sport", "jogadores__nome": "Juninho"},
-            {"time": "Sport", "jogadores__nome": "Bala"},
-            {"time": "Corinthians", "jogadores__nome": "Socrates"},
-            {"time": "Corinthians", "jogadores__nome": "Casa Grande"},
-            {"time": "Corinthians", "jogadores__nome": "Cássio"},
-        ]
-    )
+    csv = [
+        dict(time="Flamengo", jogadores__nome="Junior Baiano", jogadores_idade=40),
+        dict(time="Flamengo", jogadores__nome="Gilmar", jogadores_idade=50),
+        dict(time="Flamengo", jogadores__nome="Leão", jogadores_idade=60),
+        dict(time="Sport", jogadores__nome="Magrão", jogadores_idade=45),
+        dict(time="Sport", jogadores__nome="Juninho", jogadores_idade=55),
+        dict(time="Sport", jogadores__nome="Bala", jogadores_idade=65),
+        dict(time="Corinthians", jogadores__nome="Socrates"),
+        dict(time="Corinthians", jogadores__nome="Casa Grande"),
+        dict(time="Corinthians", jogadores__nome="Cássio", jogadores_idade=40),
+    ]
+    print(list(tabulate(flatten(json))))
+    assert list(tabulate(flatten(json))) == csv
 
-    m = MultiValueDict()
-    for k, v in flatten(json):
-        #m.appendlist(k, v)
-        print(k, v)
+    g = tabulate(flatten(json))
+    row = next(g)
+    print(list(row.keys()))
+    print(list(row.values()))
+    for row in g:
+        print(list(row.values()))
+
+
+
+
 
     #print(m)
     #assert defaultdict(list, flatten(json)) == csv
